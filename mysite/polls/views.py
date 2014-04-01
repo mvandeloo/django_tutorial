a7a62fb8e5856405e812068a8d41ffca1497d4d2
mvandeloo <martin.vandeloo@googlemail.com>
2014-04-02 00:15:22 +0200

    indentation error fixed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,24 +10,24 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-    """
-    Return the last five published polls (not including those set to be
-    published in the future).
-    """
-    return Poll.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
+        """
+        Return the last five published polls (not including those set to be
+        published in the future).
+        """
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
+    
     def get_queryset(self):
-    """
-  	Excludes any polls that aren't published yet.
-    """
-	return Poll.objects.filter(pub_date__lte=timezone.now())
-
+        """
+        Excludes any polls that aren't published yet.
+        """
+        return Poll.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Poll
