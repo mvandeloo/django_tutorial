ab81081ba9f15ca7fd998f5a9270232baee7e3de
mvandeloo <martin.vandeloo@googlemail.com>
2014-04-02 00:03:50 +0200

    tests and stylesheet added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,37 @@
-
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 from polls.models import Choice, Poll
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+    """
+    Return the last five published polls (not including those set to be
+    published in the future).
+    """
+    return Poll.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+    """
+  	Excludes any polls that aren't published yet.
+    """
+	return Poll.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
@@ -27,14 +52,7 @@ def vote(request, poll_id):
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
 
-from django.shortcuts import render, get_object_or_404
-
-from polls.models import Poll
 # ...
 def detail(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
